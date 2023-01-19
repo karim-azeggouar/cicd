4a066c747ab92e515c4003007eb177364a49186b
Azeggouar <k.azeggouar@gmail.com>
2023-01-19 14:04:48 +0100

    add title 2 and p

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
      <h1>Hello Full Stack 204 and 205  </h1>
+     <h2> Tithe 2</h2>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptates reprehenderit, voluptatibus nisi velit consequuntur aliquam a, dolor nobis natus magnam ducimus, consequatur cum tenetur. Commodi corrupti debitis aspernatur est?</p>
     </div>
   );
 }
